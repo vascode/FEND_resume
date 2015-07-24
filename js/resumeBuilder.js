d969b0dcad2787c7c52098a7988dd37b5b657110
vascode <vascodepssn@gmail.com>
2015-07-23 21:15:20 -0400

    practice .repalce with jQuery

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+$("#main").append(["seokwoo yoon"]);
+
+var awesomeThoughts = "I am seokwoo yoon and I am AWESOME!\n";
+$("#main").append(awesomeThoughts);
+
+funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("main").append(funThoughts);
