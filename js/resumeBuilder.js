64e90d9627a7651f4c60f2c68994fd06bdc6c24c
vascode <vascodepssn@gamil.com>
2015-07-24 16:13:08 -0400

    replace %data% in HTMLheaderName/HTMLheaderRole and append/prepend

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,4 @@
+/*
 $("#main").append(["seokwoo yoon"]);
 
 var awesomeThoughts = "I am seokwoo yoon and I am AWESOME!\n";
@@ -5,3 +6,13 @@ $("#main").append(awesomeThoughts);
 
 funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
 $("main").append(funThoughts);
+*/
+
+var name = "seokwoo yoon";
+var role = "web developer";
+
+var formattedName = HTMLheaderName.replace("%data%", name );
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
