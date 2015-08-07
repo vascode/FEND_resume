6edf3215fbe000d429169e13453254e79b48b7cb
vascode <vascodepssn@gmail.com>
2015-08-07 15:32:37 -0400

    remove test folder

--- test/test4.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $(".progress").each(function() {
-        var progressValue = $(this).data("value");
-        $(this).progressbar({
-            value: progressValue
-        }).children("span").appendTo(this);
-    });
-});
