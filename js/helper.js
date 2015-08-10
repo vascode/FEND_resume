8291dd82512ed1ff758486b7e68dfc63b88ba8f3
unknown <seokwoo.yoon@AKROLFQC74X1.corp.lairdtech.com>
2015-08-10 11:45:26 -0400

    fixed map layout problem

--- js/helper.js
+++ js/helper.js
@@ -118,7 +118,7 @@ function initializeMap() {
   };
 
 
-  map = new google.maps.Map(document.querySelector('#map-div'), mapOptions);
+  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
 
   /*
